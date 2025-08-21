29fc61076cff1c647324fe03a48e670bc1b9e226
EgorServer <egorkak1408@gmail.com>
2025-08-21 22:05:23 +0300

    Доработка на сервере

--- token_file.py
+++ token_file.py
@@ -3,7 +3,7 @@ import os
 def read(path):
     with open(path, 'r', encoding='utf-8') as file:
         token = file.read()
-        return token
+    return token
 
 def read_file():
     if os.uname().nodename == "PK":
@@ -11,6 +11,6 @@ def read_file():
         token = read(path_to_token)
     else:
         path_to_token = '/home/token/home_token.txt'
-        token = read(path_to_token)[-1]
+        token = read(path_to_token)[:-1]
     return token
 
