ef265d1f939141d8080f2044519ce01dffa0434e
Egor <egorkak1408@gmail.com>
2025-08-21 21:48:42 +0300

    Переделал файл token.py для работы с отладкой

--- token_file.py
+++ token_file.py
@@ -1,4 +1,16 @@
-def read_file():
-    with open('/home/token/home_token.txt', 'r', encoding='utf-8') as file:
+import os
+
+def read(path):
+    with open(path, 'r', encoding='utf-8') as file:
         token = file.read()
+        return token
+
+def read_file():
+    if os.uname().nodename == "PK":
+        path_to_token = '/home/egor/token/test.txt'
+        token = read(path_to_token)
+    else:
+        path_to_token = '/home/token/home_token.txt'
+        token = read(path_to_token)[-1]
     return token
+
